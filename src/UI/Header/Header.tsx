e45167825bb0074c96f97ccb81651bb0e2f3b577
RasidaSydykova <sydykova.rasida@mail.ru>
2024-04-30 20:15:17 +0600

    Refactored Header responsive

--- src/UI/Header/Header.tsx
+++ src/UI/Header/Header.tsx
@@ -6,9 +6,11 @@ import NavList from './components/NavList/NavList';
 
 const Header = () => {
   const [open, setOpen] = useState<boolean>(false);
-  const [showBackground, setShowBackground] = useState(false);
+  const [showBackground, setShowBackground] = useState<boolean>(false);
+  const [hideMobileMenu, setHideMobileMenu] = useState(false);
 
   const TOP_OFFSET = 50;
+  const HIDE_MENU_SCROLL = 6400;
 
   useEffect(() => {
     const handleScroll = () => {
@@ -16,6 +18,12 @@ const Header = () => {
         setShowBackground(true);
       } else {
         setShowBackground(false);
+      }
+
+      if (scrollY >= HIDE_MENU_SCROLL) {
+        setHideMobileMenu(true);
+      } else {
+        setHideMobileMenu(false);
       }
     };
 
@@ -26,9 +34,21 @@ const Header = () => {
     };
   }, []);
 
+  const handleMenuToggle = () => {
+    setOpen(!open);
+
+    document.body.style.overflow = open ? 'auto' : 'hidden';
+  };
+
   return (
     <div className="header">
-      <div className="desktop" style={{ backgroundColor: showBackground ? '#111111' : 'inherit' }}>
+      <div
+        className="desktop"
+        style={{
+          backgroundColor: showBackground ? '#111111' : 'inherit',
+          display: hideMobileMenu ? 'none' : 'block',
+        }}
+      >
         <div className="border">
           <div className="container">
             <HeaderTop />
@@ -41,7 +61,7 @@ const Header = () => {
 
       <div className="mobile">
         <div className="burger">
-          <img src="/icons/header/burger-menu.svg" alt="Menu" onClick={() => setOpen(!open)} />
+          <img src="/icons/header/burger-menu.svg" alt="Menu" onClick={handleMenuToggle} />
         </div>
         <div className={open ? 'menu active' : 'menu'}>
           <NavList />
